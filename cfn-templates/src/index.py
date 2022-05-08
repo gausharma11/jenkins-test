fd18a136378e3c753c29d10e6fba367670295deb
Gaurav Sharma <gau.sharma11@gmail.com>
2022-05-08 20:28:00 +0530

    Update index.py

--- cfn-templates/src/index.py
+++ cfn-templates/src/index.py
@@ -5,7 +5,7 @@ ec2 = boto3.resource('ec2')
 
 
 def lambda_handler(event, context):
-    filters = [{'Name': 'instance-state-name', 'Values': [*]}]
+    filters = [{'Name': 'instance-state-name', 'Values': ['*']}]
     instances = ec2.instances.filter(Filters=filters)
     RunningInstances = []
 
