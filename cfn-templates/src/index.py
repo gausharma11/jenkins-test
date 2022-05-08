d5160fdfa47a92fb02a973c5e48cfb6b0c76cc82
Gaurav Sharma <gau.sharma11@gmail.com>
2022-05-08 19:29:10 +0530

    Create index.py

---
+++ cfn-templates/src/index.py
@@ -0,0 +1,17 @@
+import json
+import boto3
+
+ec2 = boto3.resource('ec2')
+
+
+def lambda_handler(event, context):
+    filters = [{'Name': 'instance-state-name', 'Values': [*]}]
+    instances = ec2.instances.filter(Filters=filters)
+    RunningInstances = []
+
+    for instance in instances:
+        RunningInstances.append(instance.id)
+
+    instanceList = json.dumps(RunningInstances)
+
+    return{"statusCode": 200, "body": instanceList}
